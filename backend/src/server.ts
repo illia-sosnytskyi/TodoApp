8729440be29f0801eb662eac0970d0a5adfc039e
illia-sosnytskiy <kdcompany2018@gmail.com>
2022-12-14 13:57:50 +0200

    deploying on netlify

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,45 @@
 import express from 'express';
 import cors from 'cors';
 
-import { router as todosRouter } from './routes/todos';
+import serverless from 'serverless-http';
+import * as todoController from './controllers/todos';
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
-app.use('/todos', express.json(), todosRouter);
 
-app.listen(5000);
+router.get('/', (req, res) => {
+  res.json({
+    "hello": "hello"
+  });
+});
+
+router.get('/todos/', todoController.getAll);
+
+router.get('/todos/:todoId', todoController.getOne);
+
+router.post('/todos/', express.json(), todoController.postTodo);
+
+router.delete('/todos/:todoId', todoController.removeTodo);
+
+router.put('/todos/:todoId', express.json(), todoController.updateTodo);
+
+const hasAcion = (action: string) => {
+  return (req, res, next) => {
+    if (req.query.action === action) {
+      next();
+    } else {
+      next('route');
+    }
+  };
+};
+
+router.patch('/todos/', hasAcion('delete'), express.json(), todoController.removeMany);
+router.patch('/todos/', hasAcion('update'), express.json(), todoController.updateMany);
+
+app.use('/.netlify/functions/server', router);
+
+// app.listen(5000);
+
+export const handler = serverless(app);
